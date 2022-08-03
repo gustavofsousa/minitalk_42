be440a2d2af19a9f167667616eb58da054c94332
gusousa <gfsousa42@gmail.com>
2022-08-03 14:14:28 -0300

    Done function convert binary

--- client.c
+++ client.c
@@ -6,20 +6,48 @@
 /*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 15:01:31 by gusousa           #+#    #+#             */
-/*   Updated: 2022/08/02 15:22:56 by gusousa          ###   ########.fr       */
+/*   Updated: 2022/08/03 14:14:06 by gusousa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
+#include <stdio.h>
+
+int	*convert_int_binary(int num)
+{
+	static int	bytes[8];
+	int	i;
+
+	i = -1;
+	while (++i < 8)
+	{
+		bytes[7 - i] = num % 2;
+		num /= 2;
+	}
+	return (bytes);
+}
+
+
 
 int	main(int argc, char **argv)
 {
+	int	i = -1;
+	int	*byte;
+
+	byte = convert_int_binary('a');
+	while (++i < 8)
+		printf("%d", byte[i]);
+
+	/*
 	if (argc != 3 || !ft_str_is_numeric(argv[1]))
 	{
 		ft_printf("Bad. Send it right next time");
 		break	;
 	}
-	sigaction(SIGUSR1, handle_sig1);
-	sigaction(SIGUSR2, handle_sig2);
+	*/
+	//sigaction(SIGUSR1, handle_sig1);
+	//sigaction(SIGUSR2, handle_sig2);
 	
 }
+
+
